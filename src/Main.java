86de018dfbe4edee7087392414ab97a96715bdc1
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-13 15:00:20 +0900

    map구현

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,70 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Scanner sc = new Scanner(System.in);
+        int number = 1;
+        String collection = sc.next();
+        sc.nextLine();        //next의 개행문자 읽기
+        String recipeName = sc.nextLine();
+
+
+        //List 사용 시
+        if(collection.equals("List")){
+            List<String> lists = new ArrayList<>();
+            while(true){
+                String recipe = sc.nextLine();
+                if(recipe.equals("끝")){
+                    break;
+                }
+                else{
+                    lists.add(recipe);
+                }
+            }
+            System.out.println("[ " + collection + " 으로 저장된 " + recipeName + " ]");
+            for(String list : lists){
+                System.out.println(number + ". " + list);
+                number++;
+            }
+        }
+
+        //Set 사용 시
+        else if(collection.equals("Set")){
+            Set<String> sets = new HashSet<>();
+            while(true){
+                String recipe = sc.nextLine();
+                if(recipe.equals("끝")){
+                    break;
+                }
+                else{
+                    sets.add(recipe);
+                }
+            }
+            System.out.println("[ " + collection + " 으로 저장된 " + recipeName + " ]");
+            for(String set : sets){
+                System.out.println(number + ". " + set);
+                number++;
+            }
+        }
+//Map 사용 시
+        else if(collection.equals("Map")){
+            Map<Integer, String> map = new HashMap<>();
+            while(true){
+                String recipe = sc.nextLine();
+                if(recipe.equals("끝")){
+                    break;
+                }
+                else{
+                    map.put(number, recipe);
+                    number++;
+                }
+            }
+            System.out.println("[ " + collection + " 으로 저장된 " + recipeName + " ]");
+            for (Integer key : map.keySet()) {
+                System.out.println(key + ". " + map.get(key));
+            }
+        }
     }
 }
