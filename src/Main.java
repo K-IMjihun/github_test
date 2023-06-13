57f6d776d03de1a490b8e371229012143114132a
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-13 14:32:44 +0900

    Collection 입력 구현

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int number = 1;
+        String collection = sc.next();
+        sc.nextLine();        //next의 개행문자 읽기
+        String recipeName = sc.nextLine();
     }
 }
