9a3d20f9c1bc6fd41ff70fe434566395e3c44ac4
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-13 14:37:06 +0900

    list 구현

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //List 사용 시
+        if(collection.equals("List")){
+            List<String> lists = new ArrayList<>();
+            while(true){
+                String recipe = sc.nextLine();
+                if(recipe.equals("끝")){
+                    break;
+                }
+                else{
+                    lists.add(recipe);
+                }
+            }
+            System.out.println("[ " + collection + " 으로 저장된 " + recipeName + " ]");
+            for(String list : lists){
+                System.out.println(number + ". " + list);
+                number++;
+            }
+        }
     }
 }
